699c08da36a809a7fa7ff4c468b106d44d8e57a9
Avni Kothari <avni@8thlight.com>
2022-04-28 13:57:32 -0700

    added plots for variance vs confidence difference

--- src/main.py
+++ src/main.py
@@ -13,16 +13,6 @@ SETS = config['sets']
 ROOT_DIR = os.path.dirname(os.getcwd())
 EXPERIMENT_DIR = ROOT_DIR + config['experiment_dir']
 NUM_ITER = config['num_runs']
-#
-# BASE_BATCH_SIZE = config['base']['batch_size']
-# BASE_EPOCHS = config['base']['epochs']
-# BASE_LR = config['base']['lr']
-# BASE_LR_DECAY = config['base']['lr_decay']
-#
-# ATTACK_BATCH_SIZE = config['attack']['batch_size']
-# ATTACK_EPOCHS = config['attack']['epochs']
-# ATTACK_LR = config['attack']['lr']
-# ATTACK_LR_DECAY = config['attack'['lr_decay']
 
 BASE = config['base_hyperparams']
 ATTACK = config['attack_hyperparams']
